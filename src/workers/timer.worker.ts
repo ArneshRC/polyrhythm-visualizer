d3f38640d8f1efcdf84d5d58ae70d0ba7730bbcf
ArneshRC <84434238+ArneshRC@users.noreply.github.com>
2023-08-17 18:44:25 +0530

    changed ev -> event

--- src/workers/timer.worker.ts
+++ src/workers/timer.worker.ts
@@ -1,10 +1,10 @@
 let timerID: number | null = null;
 const interval = 25;
 
-self.addEventListener("message", ev => {
-    if (ev.data == "start") {
+self.addEventListener("message", event => {
+    if (event.data == "start") {
         timerID = self.setInterval(() => postMessage("tick"), interval);
-    } else if (ev.data == "stop") {
+    } else if (event.data == "stop") {
         if (timerID != null) clearInterval(timerID);
         timerID = null;
     }
